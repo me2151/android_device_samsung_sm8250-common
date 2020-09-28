ab03432bbd563384679fb94252c42c9989cce3d4
Griffin Millender <griffin.millender@copperhead.co>
2020-09-28 10:41:53 -0500

    sm8250-common: add c2q
    Change-Id: I45972b60cde7350bf711cb1a4d2185166c765140

--- Android.mk
+++ Android.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter x1q y2q z3q,$(TARGET_DEVICE)),)
+ifneq ($(filter x1q y2q z3q c2q,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
