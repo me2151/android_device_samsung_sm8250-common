f9981c033ae46a71f98a8f202c70c3745965b018
Michael Benedict <michaelbt@live.com>
2021-01-05 16:49:59 +0700

    sm8150-common: Decommonnize vendor_overlay config.fs
    Signed-off-by: Michael Benedict <michaelbt@live.com>

--- config.fs
+++ config.fs
@@ -57,15 +57,3 @@ mode: 0771
 user: AID_SYSTEM
 group: AID_SYSTEM
 caps: 0
-
-[system/product/vendor_overlay/*/bin/*]
-mode: 0755
-user: AID_ROOT
-group: AID_SHELL
-caps: 0
-
-[product/vendor_overlay/*/bin/*]
-mode: 0755
-user: AID_ROOT
-group: AID_SHELL
-caps: 0
