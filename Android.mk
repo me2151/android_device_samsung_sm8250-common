2ffceeac11309c6976beae444ef62f7c4b55cb4d
klabit87 <keith.labit@gmail.com>
2021-03-12 19:19:54 -0600

    sm8150-common: Add beyond*qlte, d*q, and r5q usa variant support

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter r5q f1q f2q,$(TARGET_DEVICE)),)
+ifneq ($(filter r5q r5que beyond0qlte beyond1qlte beyond2qlte d1q d2q,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
