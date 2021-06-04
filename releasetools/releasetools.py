3fe1052986fd531e2ce55a3cc26786aee9641051
me2151 <dillonbrosche@gmail.com>
2021-06-04 08:43:31 +0000

    Update releasetools.py for sm8250

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,25 +1,9 @@
-#
-# Copyright (C) 2019 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+  
+# SPDX-License-Identifier: Apache-2.0
+# Copyright (C) 2020 The LineageOS Project
 
 import common
-
-def FullOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
-
-def IncrementalOTA_InstallEnd(info):
-  OTA_InstallEnd(info)
+import re
 
 def AddImage(info, basename, dest):
   path = "IMAGES/" + basename
@@ -34,3 +18,18 @@ def AddImage(info, basename, dest):
 def OTA_InstallEnd(info):
   AddImage(info, "dtbo.img", "/dev/block/bootdevice/by-name/dtbo")
   AddImage(info, "vbmeta.img", "/dev/block/bootdevice/by-name/vbmeta")
+  return
+
+def FullOTA_Assertions(info):
+  return
+
+def IncrementalOTA_Assertions(info):
+  return
+
+def FullOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+  return
+
+def IncrementalOTA_InstallEnd(info):
+  OTA_InstallEnd(info)
+  return
